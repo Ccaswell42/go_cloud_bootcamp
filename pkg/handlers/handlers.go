34a1528419c075510bfa9bc0c1de5a0b66b0b5fc
Ccaswell42 <dmitriykuzin11@yandex.ru>
2023-02-28 18:54:34 +0300

    add delete method

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -74,3 +74,30 @@ func (s ServerPlaylist) AddSong(ctx context.Context, song *api.Song) (*api.Respo
 	s.Controller.AddSong(newSong)
 	return &api.Response{Message: "add new song to the playlist: " + song.Name, StatusCode: 200}, nil
 }
+
+func (s ServerPlaylist) GetCurrentSong(ctx context.Context, empty *api.Empty) (*api.Response, error) {
+	song, err := s.Controller.CurrentSong()
+	if err != nil {
+		return &api.Response{Message: err.Error(), StatusCode: 500}, err
+	}
+	if song.IsPlaying == false {
+		return &api.Response{Message: "no song is playing now", StatusCode: 200}, nil
+	}
+	return &api.Response{Message: "now playing is: " + song.Name, StatusCode: 200}, nil
+
+}
+
+func (s ServerPlaylist) DeleteSong(ctx context.Context, song *api.Song) (*api.Response, error) {
+	targetSong := playlist.Song{Name: song.Name}
+	durationStr := strconv.Itoa(int(song.Duration)) + "s"
+	durationTime, err := time.ParseDuration(durationStr)
+	if err != nil {
+		return &api.Response{Message: "can't add song", StatusCode: 500}, errors.New("can't add song")
+	}
+	targetSong.Duration = durationTime
+	err = s.Controller.DeleteSong(targetSong)
+	if err != nil {
+		return &api.Response{Message: err.Error(), StatusCode: 400}, err
+	}
+	return &api.Response{Message: "deleted: " + song.Name, StatusCode: 200}, nil
+}
